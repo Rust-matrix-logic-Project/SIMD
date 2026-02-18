822b467e205d8b442ad9c8c37f77f8671db061c8
Your Name <you@example.com>
2026-02-18 14:26:39 +0900

    csvのフォーマット見直しと計算方法の見直しを実施

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,26 @@ unsafe fn foo(){
    let nanos = {
         let start_time = Instant::now();
         for _ in 0..100_000_000 {
-        _mm256_add_epi64(_mm256_set1_epi64x(100), _mm256_set1_epi64x(100));
+        let results = _mm256_add_epi64(_mm256_set1_epi64x(100), _mm256_set1_epi64x(100));
+        let  res = results;
+        black_box(res);
         }
+        
         black_box(start_time.elapsed().as_nanos())
     };
     let mut  writer = OpenOptions::new();
-    let mut file = writer.write(true).create(true).append(true).open("test.csv").expect("書き込みが正常に行われませんでした。");
-    let result = format!("add_epi64: {:?}ナノ秒\n", nanos);
+    let mut header = writer.write(true).create(true).append(true).open("tests/add_test.csv").expect("書き込み先のファイルがありません。");
+    let mut file = writer.write(true).create(true).append(true).open("tests/add_test.csv").expect("書き込みが正常に行われませんでした。");
+    let result = format!("add_epi64, {:?}\n", nanos);
+    if header.metadata().expect("バイト数の読み込みに失敗しました。").len() == 0{
+        header.write_all(b"method, result\n").expect("書き込みに失敗しました。");
+    }
     file.write_all(result.as_bytes()).expect("ファイルへの書き込みに失敗しました。");
 
 
 }
 fn main() {
-    println!("Hello, world!");
+    println!("test start");
     unsafe {
         if is_x86_feature_detected!("avx2"){
             foo();
