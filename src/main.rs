d49aaf02000f2c4fd0e68178c81ba70b92735ec9
Your Name <you@example.com>
2026-02-18 15:26:17 +0900

    乗算のベンチマークテストを実施

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,39 @@ unsafe fn foo(){
    let nanos = {
         let start_time = Instant::now();
         for _ in 0..100_000_000 {
-        let results = _mm256_add_epi64(_mm256_set1_epi64x(100), _mm256_set1_epi64x(100));
-        let  res = results;
+        let add_results = _mm256_add_epi64(_mm256_set1_epi64x(100), _mm256_set1_epi64x(100));
+        let  res1 = add_results;
+        black_box(res1);
+        }
+        
+        black_box(start_time.elapsed().as_nanos());
+
+        let start_time = Instant::now();
+        for _ in 0..100_000_000 {
+        let mul_results = _mm256_mul_epi32(_mm256_set1_epi64x(100), _mm256_set1_epi64x(100));
+        let  res = mul_results;
         black_box(res);
         }
         
         black_box(start_time.elapsed().as_nanos())
+
     };
     let mut  writer = OpenOptions::new();
-    let mut header = writer.write(true).create(true).append(true).open("tests/add_test.csv").expect("書き込み先のファイルがありません。");
-    let mut file = writer.write(true).create(true).append(true).open("tests/add_test.csv").expect("書き込みが正常に行われませんでした。");
-    let result = format!("add_epi64, {:?}\n", nanos);
-    if header.metadata().expect("バイト数の読み込みに失敗しました。").len() == 0{
-        header.write_all(b"method, result\n").expect("書き込みに失敗しました。");
+    let mut add_header = writer.write(true).create(true).append(true).open("tests/add_test.csv").expect("書き込み先のファイルがありません。");
+    let mut add_file = writer.write(true).create(true).append(true).open("tests/add_test.csv").expect("書き込みが正常に行われませんでした。");
+    let add_result = format!("add_epi64, {:?}\n", nanos);
+    if add_header.metadata().expect("バイト数の読み込みに失敗しました。").len() == 0{
+        add_header.write_all(b"method, result\n").expect("書き込みに失敗しました。");
     }
-    file.write_all(result.as_bytes()).expect("ファイルへの書き込みに失敗しました。");
+    add_file.write_all(add_result.as_bytes()).expect("ファイルへの書き込みに失敗しました。");
 
+    let mut mul_header = writer.write(true).create(true).append(true).open("tests/mul_test.csv").expect("書き込み先のファイルがありません。");
+    let mut mul_file = writer.write(true).create(true).append(true).open("tests/mul_test.csv").expect("書き込みが正常に行われませんでした。");
+    let mul_result = format!("mul_epi32, {:?}\n", nanos);
+    if mul_header.metadata().expect("バイト数の読み込みに失敗しました。").len() == 0{
+        mul_header.write_all(b"method, result\n").expect("書き込みに失敗しました。");
+    }
+    mul_file.write_all(mul_result.as_bytes()).expect("ファイルへの書き込みに失敗しました。");
 
 }
 fn main() {
