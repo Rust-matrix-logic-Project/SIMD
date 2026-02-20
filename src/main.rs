4e6864d6df7c23e8d7d723f3146dcc992f56d24f
Your Name <you@example.com>
2026-02-20 12:39:48 +0900

    自作関数とコンパイラのベンチマーク比較を実施

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,16 @@ fn main() {
             let t =  create_tensor(3, 50);
             let result = tensor_calc(t, 179);
             println!("{:?}", result.data);
-            foo();
             let t1 = create_tensor(9, 5);
             let t2 = create_tensor(9, 5);
             let t1 = tensor_calc(t1, 14);
             let t2 = tensor_calc(t2, 784);
             let add_result= add_tensor(t1, t2);
             println!("{:?}", add_result.data);
+            let t1 = create_tensor(10000, 10000);
+            let t2 = create_tensor(10000, 10000);
+            tesor_add_benchmark(t1, t2);
+            foo();
         }else {
             eprintln!("avx2がCPUによってサポートされていません。");
             return;           
